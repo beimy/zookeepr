009a9cb3c749078a681f8b6df3ccedbabb29d7f6
Beimy <arbm159357@gmail.com>
2022-05-03 23:59:21 -0400

    linked front end content to backend server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
  // parse incoming string or array data
  app.use(express.urlencoded({ extended: true }));
 
@@ -113,6 +115,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.sendStatus(404);
     } 
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 //POST REQUESTS
